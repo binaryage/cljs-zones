41c3246cef90eb4edb4c184bae8cb80a85bf0651
Antonin Hildebrand <antonin@hildebrand.cz>
2016-08-09 13:23:14 +0200

    clean tests up a bit

--- test/src/tests/zones/tests/core.cljs
+++ test/src/tests/zones/tests/core.cljs
@@ -5,17 +5,17 @@
 
 ; -- helpers ----------------------------------------------------------------------------------------------------------------
 
-(defn print-default-zone [& args]
-  (apply print (concat args [": " zones/default-zone])))
+(defn print-default-zone [location]
+  (print (str location ": ") zones/default-zone))
 
 ; -- T0 ---------------------------------------------------------------------------------------------------------------------
 
 (defn test0 []
-  (print-default-zone "before")
-  (binding [v "I'm a dynamically bound value in default zone"]
-    (print-default-zone "inside")
-    (js/setTimeout (bound-fn* #(print-default-zone "in async call")) 500))
-  (print-default-zone "after"))
+  (print-default-zone "test0: before binding")
+  (binding [v "I'm a dynamically bound value in the default zone"]
+    (print-default-zone "test0: inside binding")
+    (js/setTimeout (bound-fn* #(print-default-zone "test0: in async call")) 500))
+  (print-default-zone "test0: after binding"))
 
 (deftest T0
   (async done
@@ -24,24 +24,24 @@
 
 ; -- T1 ---------------------------------------------------------------------------------------------------------------------
 
-(defn async-t1-fn2 []
-  (print-default-zone "T1 timeout in async-t1-fn2"))
+(defn test1-async-fn2 []
+  (print-default-zone "test1-async-fn2"))
 
-(defn async-t1-fn1 []
-  (print-default-zone "T1 async-t1-fn1 before")
+(defn test1-async-fn1 []
+  (print-default-zone "test1-async-fn1: before binding")
   (binding [v3 3
             v2 42]
-    (print-default-zone "T1 async-fn1 second-level-binding")
-    (js/setTimeout (bound-fn* async-t1-fn2) 500))
-  (print-default-zone "T1 async-fn1 after"))
+    (print-default-zone "test1-async-fn1: second-level-binding")
+    (js/setTimeout (bound-fn* test1-async-fn2) 500))
+  (print-default-zone "test1-async-fn1: after binding"))
 
 (defn test1 []
-  (print-default-zone "T1 before")
+  (print-default-zone "test1: before binding")
   (binding [v1 1
             v2 2]
-    (print-default-zone "T1 inside")
-    (js/setTimeout (bound-fn* async-t1-fn1) 1000))
-  (print-default-zone "T1 after"))
+    (print-default-zone "test1: inside binding")
+    (js/setTimeout (bound-fn* test1-async-fn1) 1000))
+  (print-default-zone "test1: after binding"))
 
 (deftest T1
   (async done
@@ -50,28 +50,29 @@
 
 ; -- T2 ---------------------------------------------------------------------------------------------------------------------
 
-(defn async-t2-fn1 []
-  (print-default-zone "T2 async-t2-fn1 before set")
+(defn test2-async-fn1 []
+  (print-default-zone "test2-async-fn1: before set")
   (set x 100)
-  (print-default-zone "T2 async-t2-fn1 after set"))
+  (print-default-zone "test2-async-fn1: after set"))
 
-(defn async-t2-fn2 []
-  (print-default-zone "T2 async-t2-fn2"))
+(defn test2-async-fn2 []
+  (print-default-zone "test2-async-fn2"))
 
-(defn async-t2-fn3 []
-  (print-default-zone "T2 async-t2-fn3"))
+(defn test2-async-fn3 []
+  (print-default-zone "test2-async-fn3"))
 
-(defn async-t2-fn4 []
-  (print-default-zone "T2 async-t2-fn4"))
+(defn test2-async-fn4 []
+  (print-default-zone "test2-async-fn4"))
 
 (defn test2 []
   (binding [x 1]
-    (print-default-zone "T2 inside")
-    (js/setTimeout (bound-fn* async-t2-fn1) 200)
-    (js/setTimeout (bound-fn* async-t2-fn2) 400)
+    (print-default-zone "test2: inside binding1")
+    (js/setTimeout (bound-fn* test2-async-fn1) 200)
+    (js/setTimeout (bound-fn* test2-async-fn2) 400)
     (binding [x 1000]
-      (js/setTimeout (bound-fn* async-t2-fn3) 300)
-      (js/setTimeout (bound-fn* async-t2-fn4) 600))))
+      (print-default-zone "test2: inside binding2")
+      (js/setTimeout (bound-fn* test2-async-fn3) 300)
+      (js/setTimeout (bound-fn* test2-async-fn4) 600))))
 
 (deftest T2
   (async done
